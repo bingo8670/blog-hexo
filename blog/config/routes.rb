18c33ba3374b6c944ed1ee79116c9fe04c457660
bingo8670 <307286130@qq.com>
2017-08-30 22:17:21 +0800

    add comment

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :articles
+  resources :articles do
+  	resources :comments
+  end
   
   root 'welcome#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # 这是应用的路由文件，使用特殊的 DSL（Domain-Specific Language，领域专属语言）编写，告诉 Rails 把访问请求发往哪个控制器和动作。
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html 这是应用的路由文件，使用特殊的 DSL （Domain-
+  # Specific Language，领域专属语言）编写，告诉 Rails 把访问请求发往哪个控制器和动作。
 end
